f727a85402c2f7339782b26e24a458a7919b83e3
Karol Jaramillo <karoljaramillo195@gmail.com>
2025-04-25 15:51:46 -0500

    Changes to be committed: 	modified:   CalculadoraFig.java

--- CalculadoraFig.java
+++ CalculadoraFig.java
@@ -1,19 +1,36 @@
 import java.util.Scanner;
-
 public class CalculadoraFig {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int figura, calculo;
 
-        system.out.println("Selecciona la figura para calcular su area o perimetro:");
-        system.out.println("1. Círculo");
+        System.out.println("Selecciona la figura para calcular su area o perimetro:");
+        System.out.println("1. Círculo");
         System.out.println("2. Cuadrado");
         System.out.println("3. Triángulo");
         System.out.println("4. Rectángulo");
         System.out.println("5. Pentágono");
         figura = scanner.nextInt();
 
-        system.out.println("Elije que deseas calcular:");
-        system.out.println("1. Área");
+        System.out.println("Elije que deseas calcular:");
+        System.out.println("1. Área");
         System.out.println("2. Perímetro");
         calculo = scanner.nextInt();
+        double total = 0; 
+
+        switch (figura) {
+            case 1:
+                System.out.print("Introduce el radio: ");
+                double radio = scanner.nextDouble();
+                if (calculo == 1)
+                    total = Math.PI * radio * radio;
+                else
+                    total = 2 * Math.PI * radio;
+                break;
+
+            case 2:
+
+
+
+    }
+}
