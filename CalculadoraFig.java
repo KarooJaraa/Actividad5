6e5db8f7af417cb8272b3f147de909a18319b447
Karol Jaramillo <karoljaramillo195@gmail.com>
2025-04-25 15:34:54 -0500

    Changes to be committed: 	new file:   CalculadoraFig.java

---
+++ CalculadoraFig.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CalculadoraFig {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int figura, calculo;
+
+        system.out.println("Selecciona la figura para calcular su area o perimetro:");
+        system.out.println("1. Círculo");
+        System.out.println("2. Cuadrado");
+        System.out.println("3. Triángulo");
+        System.out.println("4. Rectángulo");
+        System.out.println("5. Pentágono");
+        figura = scanner.nextInt();
+
+        system.out.println("Elije que deseas calcular:");
+        system.out.println("1. Área");
+        System.out.println("2. Perímetro");
+        calculo = scanner.nextInt();
